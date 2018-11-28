05712b06346b1724fb97cae377dd9b727f64c4ef
李洪智 <123456789>
2018-11-28 19:08:53 +0800

    python

--- venv/Scripts/com/def/__init__.py
+++ venv/Scripts/com/def/__init__.py
@@ -11,12 +11,29 @@ import calendar;
         4、函数内容以冒号起始，并且缩进
         5、return [表达式] 结束函数，选择性地返回一个值给调用方。不带表达式的return相当于返回 None
 """
+"""
+    1、默认参数一定要用不可变对象，如果是可变对象，程序运行时会有逻辑错误
+    2、要注意定义可变参数和关键字参数的语法：
+        *args是可变参数，args接收的是一个tuple
+        **kw是关键字参数，kw接收的是一个dict
+    3、以及调用函数时如何传入可变参数和关键字参数的语法:
+        可变参数既可以直接传入：func(1, 2, 3)，又可以先组装list或tuple，再通过*args传入：func(*(1, 2, 3))；
+        关键字参数既可以直接传入：func(a=1, b=2)，又可以先组装dict，再通过**kw传入：func(**{'a': 1, 'b': 2})。
+    4、使用*args和**kw是Python的习惯写法，当然也可以用其他参数名，但最好使用习惯用法
+    5、命名的关键字参数是为了限制调用者可以传入的参数名，同时可以提供默认值
+    6、定义命名的关键字参数在没有可变参数的情况下不要忘了写分隔符*，否则定义的将是位置参数
+"""
 def printme( str ):
    "打印传入的字符串到标准显示设备上"
    print str
    return
 
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n-1)
 
+print fact(3)
 def getLeapYear(startYear,endYear):
     "获取两年之间的闰年"
     yearList = []
@@ -27,7 +44,6 @@ def getLeapYear(startYear,endYear):
             yearList.append(startYear)
         startYear += 1
     return yearList
-
 
 print getLeapYear(1900,2000)
 print len(getLeapYear(endYear = 2000,startYear = 1900))
